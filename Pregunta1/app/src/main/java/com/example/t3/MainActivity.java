24118ff6d4821ad4358d8b7126dfb8237c7c828a
manuelcabrera1193 <manuel.968762062@gmail.com>
2022-09-24 22:21:12 -0500

    finish t3_01

--- Pregunta1/app/src/main/java/com/example/t3/MainActivity.java
+++ Pregunta1/app/src/main/java/com/example/t3/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.t3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.t3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initEvents();
+    }
+
+    private void initEvents() {
+        binding.btnCalculate.setOnClickListener(listener -> {
+            try {
+                String valueInput = binding.input.getText().toString();
+                if (valueInput.isEmpty()) {
+                    Toast.makeText(this, "Ingrese un monto", Toast.LENGTH_LONG).show();
+                } else {
+                    double value = Double.parseDouble(valueInput);
+                    binding.result1.setText(String.format("%s", value * 0.2));
+                    binding.result2.setText(String.format("%s", value * 0.3));
+                    binding.result3.setText(String.format("%s", value * 0.5));
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Ocurrio un error al calcular.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
